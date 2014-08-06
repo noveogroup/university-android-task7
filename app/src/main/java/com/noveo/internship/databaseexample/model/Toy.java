93d478a0e1664d1e85c9ccee44475f25bafa627e
Roman Savin <roman.savin@noveogroup.com>
2014-08-06 14:37:32 +0700

    Add example project

---
+++ app/src/main/java/com/noveo/internship/databaseexample/model/Toy.java
@@ -0,0 +1,22 @@
+package com.noveo.internship.databaseexample.model;
+
+public class Toy {
+
+    private int id;
+    private String title;
+    private int cost;
+
+    public Toy(int cost, String title) {
+        this.cost = cost;
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
